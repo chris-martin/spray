chris-martin/spray

commit ed431767696071a01203f6c42ea2bb69f84082b9

use Vec3 in Measures, Intersection, Perspective

java/src/main/tube/Measures.java
@@ -1,7 +1,9 @@
 package tube;
 
+import spray.Geometry.Vec3;
+
+import static spray.Geometry.distance;
 import static tube.Vec.N;
-import static tube.Vec.V;
 
 import static processing.core.PApplet.*;
 
@@ -13,13 +15,13 @@ public class Measures {
     }
 
     // (UxV)*W  mixed product, determinant
-    public static float m(Vec U, Vec V, Vec W) {
-        return d(U, N(V, W));
+    public static float m(Vec3 U, Vec3 V, Vec3 W) {
+        return distance(U, V.cross(W));
     }
 
     // det (EA EB EC) is >0 when E sees (A,B,C) clockwise
-    public static float m(Pt E, Pt A, Pt B, Pt C) {
-        return m(V(E, A), V(E, B), V(E, C));
+    public static float m(Vec3 E, Vec3 A, Vec3 B, Vec3 C) {
+        return m(A.sub(E), B.sub(E), C.sub(E));
     }
 
     // V*V    norm squared
@@ -43,8 +45,8 @@ public class Measures {
     }
 
     // volume of tet
-    public static float volume(Pt A, Pt B, Pt C, Pt D) {
-        return m(V(A, B), V(A, C), V(A, D)) / 6;
+    public static float volume(Vec3 A, Vec3 B, Vec3 C, Vec3 D) {
+        return m(B.sub(A), C.sub(A), D.sub(A)) / 6;
     }
 
     // true if U and V are almost parallel
@@ -58,12 +60,12 @@ public class Measures {
     }
 
     // (UxV)*W>0  U,V,W are clockwise
-    public static boolean cw(Vec U, Vec V, Vec W) {
+    public static boolean cw(Vec3 U, Vec3 V, Vec3 W) {
         return m(U, V, W) > 0;
     }
 
     // tet is oriented so that A sees B, C, D clockwise
-    public static boolean cw(Pt A, Pt B, Pt C, Pt D) {
+    public static boolean cw(Vec3 A, Vec3 B, Vec3 C, Vec3 D) {
         return volume(A, B, C, D) > 0;
     }
 

java/src/main/tube/Intersection.java
@@ -1,44 +1,46 @@
 package tube;
 
+import spray.Geometry.Line3;
+import spray.Geometry.Vec3;
+
+import static spray.Geometry.aToB;
 import static tube.Measures.cw;
 import static tube.Measures.m;
-import static tube.Pt.P;
-import static tube.Vec.V;
 
 public class Intersection {
 
-    // if (P,Q) intersects (A,B,C), return true and set X to the intersection point
-    public static boolean intersect(Pt P, Pt Q, Pt A, Pt B, Pt C, Pt X) {
-        return intersect(P, V(P, Q), A, B, C, X);
+    // if (P,Q) intersects (A,B,C), return the intersection point
+    public static Vec3 intersect(Line3 PQ, Vec3 A, Vec3 B, Vec3 C) {
+        return intersect(PQ.a(), PQ.ab(), A, B, C);
     }
 
-    // if ray from E along T intersects triangle (A,B,C), return true and set X to the intersection point
-    public static boolean intersect(Pt E, Vec T, Pt A, Pt B, Pt C, Pt X) {
-        Vec EA = V(E, A), EB = V(E, B), EC = V(E, C), AB = V(A, B), AC = V(A, C);
+    // if ray from E along T intersects triangle (A,B,C), return the intersection point
+    public static Vec3 intersect(Vec3 E, Vec3 T, Vec3 A, Vec3 B, Vec3 C) {
+        Vec3 EA = aToB(E, A).ab(), EB = aToB(E, B).ab(), EC = aToB(E, C).ab(), AB = aToB(A, B).ab(), AC = aToB(A, C).ab();
         boolean s = cw(EA, EB, EC), sA = cw(T, EB, EC), sB = cw(EA, T, EC), sC = cw(EA, EB, T);
         if ((s == sA) && (s == sB) && (s == sC)) {
-            return false;
+            return null;
         }
         float t = m(EA, AC, AB) / m(T, AC, AB);
-        X.set(P(E, t, T));
-        return true;
+        return E.add(T.mult(t));
     }
 
     // true if ray from E with direction T hits triangle (A,B,C)
-    public static boolean rayIntersectsTriangle(Pt E, Vec T, Pt A, Pt B, Pt C) {
-        Vec EA = V(E, A), EB = V(E, B), EC = V(E, C);
+    public static boolean rayIntersectsTriangle(Vec3 E, Vec3 T, Vec3 A, Vec3 B, Vec3 C) {
+        Vec3 EA = aToB(E, A).ab(), EB = aToB(E, B).ab(), EC = aToB(E, C).ab();
         boolean s = cw(EA, EB, EC), sA = cw(T, EB, EC), sB = cw(EA, T, EC), sC = cw(EA, EB, T);
         return (s == sA) && (s == sB) && (s == sC);
     }
 
-    public static boolean edgeIntersectsTriangle(Pt P, Pt Q, Pt A, Pt B, Pt C) {
-        Vec PA = V(P, A), PQ = V(P, Q), PB = V(P, B), PC = V(P, C), QA = V(Q, A), QB = V(Q, B), QC = V(Q, C);
+    public static boolean edgeIntersectsTriangle(Vec3 P, Vec3 Q, Vec3 A, Vec3 B, Vec3 C) {
+        Vec3 PA = aToB(P, A).ab(), PQ = aToB(P, Q).ab(), PB = aToB(P, B).ab(), PC = aToB(P, C).ab(),
+            QA = aToB(Q, A).ab(), QB = aToB(Q, B).ab(), QC = aToB(Q, C).ab();
         boolean p = cw(PA, PB, PC), q = cw(QA, QB, QC), a = cw(PQ, PB, PC), b = cw(PA, PQ, PC), c = cw(PQ, PB, PQ);
         return (p != q) && (p == a) && (p == b) && (p == c);
     }
 
-    public static float rayParameterToIntersection(Pt E, Vec T, Pt A, Pt B, Pt C) {
-        Vec AE = V(A, E), AB = V(A, B), AC = V(A, C);
+    public static float rayParameterToIntersection(Vec3 E, Vec3 T, Vec3 A, Vec3 B, Vec3 C) {
+        Vec3 AE = aToB(A, E).ab(), AB = aToB(A, B).ab(), AC = aToB(A, C).ab();
         return -m(AE, AC, AB) / m(T, AC, AB);
     }
 

java/src/main/tube/Main.java
@@ -638,7 +638,7 @@ public class Main extends PApplet {
             if (n(N) < 0.001) {
                 L[i] = V(L[i - 1]);
             } else {
-                L[i] = A(L[i - 1], m(U(N), I, L[i - 1]), N(U(N), M(Ip, I)));
+                L[i] = A(L[i - 1], m(U(N).$, I.$, L[i - 1].$), N(U(N), M(Ip, I)));
             }
             I = U(L[i]);
             Vec J = U(N(I, Ip));

java/src/main/tube/Perspective.java
@@ -1,15 +1,17 @@
 package tube;
 
+import spray.Geometry.Vec3;
+
 import static tube.Pt.P;
 
 public class Perspective {
 
-    static Pt Pers(Pt P, float d) {
-        return P(d * P.x / (d + P.z), d * P.y / (d + P.z), d * P.z / (d + P.z));
+    static Pt Pers(Vec3 P, float d) {
+        return P(d * P.x() / (d + P.z()), d * P.y() / (d + P.z()), d * P.z() / (d + P.z()));
     }
 
-    static Pt InverserPers(Pt P, float d) {
-        return P(d * P.x / (d - P.z), d * P.y / (d - P.z), d * P.z / (d - P.z));
+    static Pt InverserPers(Vec3 P, float d) {
+        return P(d * P.x() / (d - P.z()), d * P.y() / (d - P.z()), d * P.z() / (d - P.z()));
     }
 
 }

commit 385c53de6989331dd0a3609ceea0be0d517a5674

fix bug introduced by ed43176

java/src/main/tube/Measures.java
@@ -2,10 +2,8 @@ package tube;
 
 import spray.Geometry.Vec3;
 
-import static spray.Geometry.distance;
-import static tube.Vec.N;
-
 import static processing.core.PApplet.*;
+import static tube.Vec.N;
 
 public class Measures {
 
@@ -16,7 +14,7 @@ public class Measures {
 
     // (UxV)*W  mixed product, determinant
     public static float m(Vec3 U, Vec3 V, Vec3 W) {
-        return distance(U, V.cross(W));
+        return U.dot(V.cross(W));
     }
 
     // det (EA EB EC) is >0 when E sees (A,B,C) clockwise